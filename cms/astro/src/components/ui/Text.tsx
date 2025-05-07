b2250ccf672c6b08d9b2bce867abc242943fe957
Lindsey Zylstra <lindseyzylstra@gmail.com>
2025-05-07 14:34:47 -0700

    Prose styles matches Nuxt

--- cms/astro/src/components/ui/Text.tsx
+++ cms/astro/src/components/ui/Text.tsx
@@ -10,7 +10,10 @@ export interface TextProps {
 const Text = ({ content, className, 'data-directus': dataDirectus }: TextProps) => {
   return (
     <div
-      className={cn('prose dark:prose-invert', className)}
+      className={cn(
+        'prose dark:prose-invert text-foreground prose-img:rounded-lg prose-img:border-2 prose-img:border-gray-500 prose-headings:font-display',
+        className,
+      )}
       dangerouslySetInnerHTML={{ __html: content }}
       data-directus={dataDirectus}
     />
--- cms/sveltekit/src/lib/components/ui/Text.svelte
+++ cms/sveltekit/src/lib/components/ui/Text.svelte
@@ -10,6 +10,12 @@
 	let { content, 'data-directus': dataDirectus, ...props }: TextProps = $props();
 </script>
 
-<div class={cn('prose dark:prose-invert', props.class)} data-directus={dataDirectus}>
+<div
+	class={cn(
+		'prose dark:prose-invert text-foreground prose-img:rounded-lg prose-img:border-2 prose-img:border-gray-500 prose-headings:font-display',
+		props.class
+	)}
+	data-directus={dataDirectus}
+>
 	{@html content}
 </div>
