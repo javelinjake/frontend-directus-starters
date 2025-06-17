237df275b09a5023a4bb76b487be31850b0b4538
LZylstra <lindseyzylstra@gmail.com>
2025-06-17 09:09:02 -0700

    Astro redirects (#48)
    * Astro redirects
    
    * Match to other starters

--- cms/astro/astro.config.ts
+++ cms/astro/astro.config.ts
@@ -1,12 +1,29 @@
-// @ts-check
+import 'dotenv/config';
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import vercel from '@astrojs/vercel';
+import { fetchRedirects } from './src/lib/fetchRedirects';
 
-const directusUrl = process.env.NEXT_PUBLIC_DIRECTUS_URL || process.env.DIRECTUS_URL || '';
+const directusUrl = process.env.PUBLIC_DIRECTUS_URL || process.env.DIRECTUS_URL || '';
 const directusHost = directusUrl?.split('//')[1];
-
 const siteUrl = process.env.PUBLIC_SITE_URL || 'http://localhost:4321';
+
+if (!directusUrl) {
+  throw new Error(
+    'Missing PUBLIC_DIRECTUS_URL in your .env. ' +
+    'Make sure you have PUBLIC_DIRECTUS_URL set at the project root.'
+  );
+}
+
+const redirectsArray = await fetchRedirects(directusUrl);
+const redirectsConfig: Record<string, { status: 301 | 302; destination: string }> = {};
+
+for (const { source, destination, permanent } of redirectsArray) {
+  redirectsConfig[source] = {
+    status: permanent ? 301 : 302,
+    destination,
+  };
+}
 
 export default defineConfig({
   site: siteUrl,
@@ -27,6 +44,8 @@ export default defineConfig({
       },
     ],
   },
+  redirects: redirectsConfig,
+
   vite: {
     envPrefix: ['PUBLIC_', 'DIRECTUS_'],
     assetsInclude: ['**/*.svg'],
